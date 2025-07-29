d5493192f9096c909f23678d766848173fcb35e7
verrlahh <verrisanidiot@gmail.com>
2025-07-29 22:03:23 +0800

    sleeplog

--- group members/verryn.js
+++ group members/verryn.js
@@ -40,5 +40,39 @@ const checkAdmin = (req, res, next) => {
     }
 };
 
+//verryn code
+router.get('/admin/sleeplog', checkAuthenticated, (req, res) => {
+    const sql = 'SELECT * FROM sleep_logs';
+    db.query(sql, (error, results) => {
+        if (error) {
+            console.error('Database query error:', error.message);
+            return res.status(500).send('Error retrieving sleep logs');
+        }
+
+        res.render('admin/sleeplog', { user: req.session.user, sleep_logs: results });
+    });
+});
+
+// View a single sleep log (renders views/admin/sleeplog_view.ejs)
+router.get('/admin/sleeplog/:id', checkAuthenticated, (req, res) => {
+    const id = req.params.id;
+    const sql = 'SELECT * FROM sleep_logs WHERE logId = ?';
+
+    db.query(sql, [id], (error, results) => {
+        if (error) {
+            console.error('Database query error:', error.message);
+            return res.status(500).send('Error retrieving sleep log by ID');
+        }
+        if (results.length > 0) {
+            res.render('admin/sleeplog_view', {
+                user: req.session.user,
+                sleep_log: results[0]
+            });
+        } else {
+            res.status(404).send('Sleep log not found');
+        }
+    });
+});
+
 
 module.exports = router;
